0f9c161a8ece8f3e42edfff0b9d5b441fbc5c8f8
Andrew Linnell <andrewlinnell@Andrews-MacBook-Air.local>
2023-01-11 11:55:38 -0500

    Loop code challenge

---
+++ code-challenge2.js
@@ -0,0 +1,32 @@
+// given any array of numbers write a function that returns the largest number in that array (without using math.max)
+
+myArray = [43, 12, 9, 76, 24, 55, 87, 44]
+
+function largestNum (array) {
+    let maxVal=0
+    for (i = 0; i < array.length ; i++) {
+        if (array[i] > maxVal){
+            maxVal=array[i]
+        }
+
+    }
+    return maxVal;
+}
+
+console.log(largestNum(myArray));
+
+// give count of odd numbers in an array
+
+myArray = [43, 12, 9, 76, 24, 55, 87, 44]
+
+function addOddAmount (anArray) {
+    let oddAmount=0
+    for (i = 0; i < anArray.length ; i++) 
+        
+        if (anArray[i]%2){
+            oddAmount++
+    }
+    return oddAmount;
+}
+
+console.log(addOddAmount(myArray));
