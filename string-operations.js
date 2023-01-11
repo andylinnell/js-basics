7a1ffa7fc4e28c6ad1ef7ad70469d5ac91424e9c
Andrew Linnell <andrewlinnell@Andrews-MacBook-Air.local>
2023-01-11 16:36:07 -0500

    string operations and examples

---
+++ string-operations.js
@@ -0,0 +1,34 @@
+const name = "andy";
+const favColor = "purple";
+
+//create the following sentence
+// const fullSentence = "Hello, " + name + ". Your fav color is " + favColor;
+
+// TEMPLATE LITERAL
+const fullSentence = `    Hello ${name}. Your fav color is ${favColor}`; // uses backticks
+
+// console.log("contac=>t", fullSentence);
+
+// LENGTH
+//console.log("string length", fullSentence.length);
+
+// TO UPPERCASE
+//console.log("to upper case", fullSentence.toUpperCase());
+
+// TO LOWERCASE
+//console.log("to lowercase", fullSentence.toLocaleLowerCase());
+
+//TRIM
+//console.log("time", fullSentence.trim());
+
+//REPLACE
+// const newSentence = fullSentence.replace("Hello", "Yo");
+// console.log("replace", newSentence);
+
+// SUBSTRING
+
+const otherSentence = "integer";
+console.log(otherSentence.substring(0,3));
+
+
+
