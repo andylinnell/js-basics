220206c8c52267de5938c709f26c53ed6a7659b3
Andrew Linnell <andrewlinnell@Andrews-MacBook-Air.local>
2023-01-11 11:20:35 -0500

    Loops for beginners

---
+++ code-challenge.js
@@ -0,0 +1,18 @@
+// given an array of numbers, write a function to return the sum
+
+const array = [23,74,18,42,69,91,88];
+
+
+function calculateSum (arr) {
+    
+    let sum = arr[0];
+    
+    for(let i = 1 ; i < arr.length; i++){
+        sum = sum + arr[i];
+     }
+     
+     return sum; 
+    }
+    
+const mySum = calculateSum(array);
+console.log(mySum);
